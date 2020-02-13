f305bc43abcdc99f20715c80869bd74189ee3eb1
Tom-progg <tom.wat@hotmail.com>
2020-02-13 09:27:55 +0100

    entiteCapa

--- src/main/java/be/dungeon_ucm/demo/DAL/Models/Data/CapaciteEntity.java
+++ src/main/java/be/dungeon_ucm/demo/DAL/Models/Data/CapaciteEntity.java
@@ -1,6 +1,8 @@
 package be.dungeon_ucm.demo.DAL.Models.Data;
 
 
+import be.dungeon_ucm.demo.BL.Models.EtatNature.Etat;
+import be.dungeon_ucm.demo.BL.Models.EtatNature.NatureElement;
 import lombok.*;
 
 import javax.persistence.*;
@@ -19,27 +21,29 @@ public class CapaciteEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true,name = "Nom")
+    @Column(unique = true,name = "Nom", nullable = false)
     private String nomCapacite;
 
     @Column(name="Degats",nullable = false)
     private int degats;
 
-    @Column(name = "taux_Critique")
+    @Column(name = "taux_Critique", nullable = false)
     private int tauxCritiqueCapacite;
 
-    @Column(name = "Niveau_Requis")
+    @Column(name = "Niveau_Requis", nullable = false)
     private int niveauUtilisation;
 
-    @Column(name ="Type_Capacite")
+    @Column(name ="Type_Capacite", nullable = false)
     private boolean type;
 
-    @Column(name ="forme_Capacite")
+    @Column(name ="forme_Capacite", nullable = false)
     private boolean isMagique;
 
-    @Column(name="ID_Etat")
-    private int id_Etat;
+    @Column(name="Etat", nullable = false)
+    private String etatNom;
 
-    @Column(name="Id_Nature")
-    private int id_Nature;
+    @Column(name="Nature", nullable = false)
+    private String natureNom;
+
+
 }
